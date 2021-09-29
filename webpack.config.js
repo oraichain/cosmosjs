ba2f6dfe25ffe24ab0e621335f6995e2d8e9d25b
Duc Pham Le <ducphamle212@gmail.com>
2021-09-29 09:56:05 +0700

    config cosmosjs for web run

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
+const commonConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { compact: false }
+      }
+    ]
+  },
+  // maximum 20 MB
+  performance: {
+    hints: false,
+    maxEntrypointSize: 40480000,
+    maxAssetSize: 40480000
+  }
+}
+
+const nodeConfig = {
   mode: 'production',
   entry: './src/index.js',
   target: 'node',
@@ -19,20 +38,24 @@ module.exports = {
   externals: {
     'node-fetch': 'commonjs2 node-fetch'
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: { compact: false }
-      }
-    ]
-  },
-  // maximum 20 MB
-  performance: {
-    hints: false,
-    maxEntrypointSize: 40480000,
-    maxAssetSize: 40480000
-  }
+  ...commonConfig,
 };
+
+const webConfig = {
+  mode: 'production',
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.web.js',
+    globalObject: 'this',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+    //fallback: { "buffer": false }
+  },
+  ...commonConfig,
+};
+
+module.exports = [nodeConfig, webConfig];
